93028b7b0bbbb09eb12f95137728ff1b2f7ab298
Bill Wolf <wmwolf@physics.ucsb.edu>
2018-02-26 11:32:57 -0700

    Add new helper method to format times properly.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,7 @@
 module ApplicationHelper
+  def format_time(time)
+    time_zone = 'utc'
+    time_zone = current_user.time_zone if current_user
+    I18n.l time.to_time.in_time_zone(time_zone), format: short
+  end
 end
