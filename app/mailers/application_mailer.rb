df4f40658e875c086e39f3025c87512eed896202
Bill Wolf <wolfwm@uwec.edu>
2020-10-19 18:10:37 -0500

    switch morning mailer to basic smtp using mailgun

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,14 @@
 class ApplicationMailer < ActionMailer::Base
-  default from: 'noreply@mesastar.org'
+  default from: 'mesa-developers@lists.mesastar.org'
   layout 'mailer'
 end
+
+ApplicationMailer.smtp_settings = {
+  :port           => ENV['MAILGUN_SMTP_PORT'],
+  :address        => ENV['MAILGUN_SMTP_SERVER'],
+  :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
+  :password       => ENV['MAILGUN_SMTP_PASSWORD'],
+  :domain         => 'testhub.mesastar.org',
+  :authentication => :plain
+}
+ApplicationMailer.delivery_method = :smtp
