270d80bdb509e1c1274b4c20668895b1cb15e9ff
Bill Wolf <wolfwm@uwec.edu>
2021-01-12 16:24:47 -0600

    Changes to hopefully fix js pipeline issues.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -28,10 +28,10 @@ Rails.application.configure do
 
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
-  # config.assets.css_compressor = :sass
+  config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+  config.assets.compile = true
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   # config.action_controller.asset_host = 'http://assets.example.com'
 
