8b81ad568b753b9c3efd467cda6fda7efc09528e
Bill Wolf <wolfwm@uwec.edu>
2026-05-20 01:17:07 -0400

    Phase 3: flip framework defaults 6.0 to 6.1

--- config/application.rb
+++ config/application.rb
@@ -9,7 +9,7 @@ Bundler.require(*Rails.groups)
 module MESATestHub
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 6.0
+    config.load_defaults 6.1
     config.cache_store = :memory_store, { size: 64.megabytes }
 
     # Enable rack-attack middleware (configured in initializers/rack_attack.rb)
